6dd260ab903cbb51cc3889640c1780c2519095d6
jaylee <jayginwoolee98@gmail.com>
2024-04-12 23:40:26 +0900

    [4/n] Add TodoItemList react component to display todo items: Add onSubmit callback to add newTodoItem to todoItems state

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -45,13 +45,22 @@ const TodoItemList = (props) => {
   );
 };
 
+let todoItemId = 0;
 
 function App() {
   const [todoItemList, setTodoItemList] = useState([]);
 
+  const onSubmit = (newTodoItem) => {
+    setTodoItemList([...todoItemList, {
+      id: todoItemId++,
+      todoItemContent: newTodoItem,
+      isFinished: false,
+    }]);
+  };
+
   return (
     <div className="App">
-      <TodoItemInputField onSubmit={(input) => { console.log(input) }} />
+      <TodoItemInputField onSubmit={onSubmit} />
       <TodoItemList todoItemList={todoItemList} />
     </div>
   );
