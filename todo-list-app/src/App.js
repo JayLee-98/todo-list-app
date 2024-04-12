fb561db1df710d3715cdd5a81cdbd793b213a132
jaylee <jayginwoolee98@gmail.com>
2024-04-13 01:22:45 +0900

    Integrate Firebase

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -4,6 +4,28 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD4T2sFXxYpUepLfby7_0__svccOt_57SM",
+  authDomain: "todo-list-app-c9c51.firebaseapp.com",
+  projectId: "todo-list-app-c9c51",
+  storageBucket: "todo-list-app-c9c51.appspot.com",
+  messagingSenderId: "77571397844",
+  appId: "1:77571397844:web:580110d48c2eacf5a4c341",
+  measurementId: "G-E5CTCMJYR3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const TodoItemInputField = (props) => {
 
   const [input, setInput] = useState("");
