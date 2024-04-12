fae50c8d7d550cf4dc9eaa834257453cc2710c24
jaylee <jayginwoolee98@gmail.com>
2024-04-12 23:01:58 +0900

    [3/n] Create TodoItemInputField: Add Button to TodoItemInputField component

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -1,15 +1,42 @@
 import './App.css';
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 
 const TodoItemInputField = (props) => {
-    return (<div>
-    </div>);
-  };
-  
-  
+  const [input, setInput] = useState("");
+
+  return (<div>
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField
+        id="todo-item-input"
+        label="Todo List를 입력해주세요."
+        variant="standard"
+        onChange={(e) => setInput(e.target.value)} value={input}
+      />
+      <Button variant="outlined">Submit</Button>
+
+    </Box>
+
+
+
+  </div>);
+};
+
+
 function App() {
   return (
     <div className="App">
-        <TodoItemInputField />
+      <TodoItemInputField />
     </div>
   );
 }
