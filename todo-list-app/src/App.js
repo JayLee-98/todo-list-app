47fbebcc7a014abca24632992307853609b1996e
jaylee <jayginwoolee98@gmail.com>
2024-04-13 00:05:55 +0900

    [2/n] Create TodoItem react component: Strikethrough todo item if marked finished

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -7,37 +7,45 @@ import Button from '@mui/material/Button';
 const TodoItemInputField = (props) => {
 
   const [input, setInput] = useState("");
-
   const onSubmit = () => {
     props.onSubmit(input);
     setInput("");
   };
 
   return (
-  <div>
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField
-        id="todo-item-input"
-        label="Todo List를 입력해주세요."
-        variant="standard"
-        onChange={(e) => setInput(e.target.value)} value={input}
-      />
-      <Button variant="outlined" onClick={onSubmit}>Submit</Button>
-    </Box>
-  </div>);
+    <div>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="todo-item-input"
+          label="Todo List를 입력해주세요."
+          variant="standard"
+          onChange={(e) => setInput(e.target.value)} value={input}
+        />
+        <Button variant="outlined" onClick={onSubmit}>Submit</Button>
+      </Box>
+    </div>
+  );
 };
 
 const TodoItem = (props) => {
+
+  const style = props.todoItem.isFinished ? { textDecoration: 'line-through' } : {};
+
   return (
     <li>
-      <span>{props.todoItem.todoItemContent}</span>
+      <span
+        style={style}
+        onClick={() => props.onTodoItemClick(props.todoItem)}
+      >
+        {props.todoItem.todoItemContent}
+      </span>
     </li>
   );
 };
@@ -45,7 +53,7 @@ const TodoItem = (props) => {
 const TodoItemList = (props) => {
 
   const todoList = props.todoItemList.map((todoItem, index) => {
-    return <TodoItem key={index} todoItem={todoItem}/>
+    return <TodoItem key={index} todoItem={todoItem} onTodoItemClick={props.onTodoItemClick} />
   });
 
   return (
@@ -68,10 +76,27 @@ function App() {
     }]);
   };
 
+  const onTodoItemClick = (clickedTodoItem) => {
+    setTodoItemList(todoItemList.map((todoItem) => {
+      if (clickedTodoItem.id === todoItem.id) {
+        return {
+          id: clickedTodoItem.id,
+          todoItemContent: clickedTodoItem.todoItemContent,
+          isFinished: !clickedTodoItem.isFinished,
+        };
+      } else {
+        return todoItem;
+      }
+    }));
+  };
+
   return (
     <div className="App">
       <TodoItemInputField onSubmit={onSubmit} />
-      <TodoItemList todoItemList={todoItemList} />
+      <TodoItemList
+        todoItemList={todoItemList}
+        onTodoItemClick={onTodoItemClick}
+      />
     </div>
   );
 }
